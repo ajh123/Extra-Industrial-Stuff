674d15bda013e0cd64a65b5111cc42d23ad03ebe
ajh123 <ajh123@users.noreply.github.com>
2023-10-14 19:21:46 +0100

    Some creative tab stuff

--- src/main/java/tk/minersonline/extra_industrial_stuff/init/ExtraIndustrialStuffModItems.java
+++ src/main/java/tk/minersonline/extra_industrial_stuff/init/ExtraIndustrialStuffModItems.java
@@ -23,18 +23,18 @@ import net.minecraft.world.item.BlockItem;
 
 public class ExtraIndustrialStuffModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, ExtraIndustrialStuffMod.MODID);
+	public static final RegistryObject<Item> STEEL_INGOT = REGISTRY.register("steel_ingot", () -> new SteelIngotItem());
+	public static final RegistryObject<Item> STEEL_BLOCK = block(ExtraIndustrialStuffModBlocks.STEEL_BLOCK);
+	public static final RegistryObject<Item> STEEL_CASING = block(ExtraIndustrialStuffModBlocks.STEEL_CASING);
 	public static final RegistryObject<Item> STEEL_AXE = REGISTRY.register("steel_axe", () -> new SteelAxeItem());
 	public static final RegistryObject<Item> STEEL_PICKAXE = REGISTRY.register("steel_pickaxe", () -> new SteelPickaxeItem());
 	public static final RegistryObject<Item> STEEL_SWORD = REGISTRY.register("steel_sword", () -> new SteelSwordItem());
 	public static final RegistryObject<Item> STEEL_SHOVEL = REGISTRY.register("steel_shovel", () -> new SteelShovelItem());
 	public static final RegistryObject<Item> STEEL_HOE = REGISTRY.register("steel_hoe", () -> new SteelHoeItem());
-	public static final RegistryObject<Item> STEEL_BLOCK = block(ExtraIndustrialStuffModBlocks.STEEL_BLOCK);
-	public static final RegistryObject<Item> STEEL_INGOT = REGISTRY.register("steel_ingot", () -> new SteelIngotItem());
 	public static final RegistryObject<Item> STEEL_ARMOR_HELMET = REGISTRY.register("steel_armor_helmet", () -> new SteelArmorItem.Helmet());
 	public static final RegistryObject<Item> STEEL_ARMOR_CHESTPLATE = REGISTRY.register("steel_armor_chestplate", () -> new SteelArmorItem.Chestplate());
 	public static final RegistryObject<Item> STEEL_ARMOR_LEGGINGS = REGISTRY.register("steel_armor_leggings", () -> new SteelArmorItem.Leggings());
 	public static final RegistryObject<Item> STEEL_ARMOR_BOOTS = REGISTRY.register("steel_armor_boots", () -> new SteelArmorItem.Boots());
-	public static final RegistryObject<Item> STEEL_CASING = block(ExtraIndustrialStuffModBlocks.STEEL_CASING);
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block) {
 		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties()));
--- src/main/java/tk/minersonline/extra_industrial_stuff/init/ExtraIndustrialStuffModTabs.java
+++ src/main/java/tk/minersonline/extra_industrial_stuff/init/ExtraIndustrialStuffModTabs.java
@@ -8,40 +8,27 @@ import tk.minersonline.extra_industrial_stuff.ExtraIndustrialStuffMod;
 
 import net.minecraftforge.registries.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.network.chat.Component;
 import net.minecraft.core.registries.Registries;
 
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ExtraIndustrialStuffModTabs {
 	public static final DeferredRegister<CreativeModeTab> REGISTRY = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, ExtraIndustrialStuffMod.MODID);
 	public static final RegistryObject<CreativeModeTab> EXTRA_INDUSTRIAL_STUFF = REGISTRY.register("extra_industrial_stuff",
-			() -> CreativeModeTab.builder().title(Component.translatable("item_group.extra_industrial_stuff.extra_industrial_stuff")).icon(() -> new ItemStack(Blocks.REDSTONE_WIRE)).displayItems((parameters, tabData) -> {
+			() -> CreativeModeTab.builder().title(Component.translatable("item_group.extra_industrial_stuff.extra_industrial_stuff")).icon(() -> new ItemStack(ExtraIndustrialStuffModItems.STEEL_INGOT.get())).displayItems((parameters, tabData) -> {
+				tabData.accept(ExtraIndustrialStuffModItems.STEEL_INGOT.get());
+				tabData.accept(ExtraIndustrialStuffModBlocks.STEEL_BLOCK.get().asItem());
+				tabData.accept(ExtraIndustrialStuffModBlocks.STEEL_CASING.get().asItem());
 				tabData.accept(ExtraIndustrialStuffModItems.STEEL_AXE.get());
 				tabData.accept(ExtraIndustrialStuffModItems.STEEL_PICKAXE.get());
 				tabData.accept(ExtraIndustrialStuffModItems.STEEL_SWORD.get());
 				tabData.accept(ExtraIndustrialStuffModItems.STEEL_SHOVEL.get());
 				tabData.accept(ExtraIndustrialStuffModItems.STEEL_HOE.get());
-				tabData.accept(ExtraIndustrialStuffModBlocks.STEEL_BLOCK.get().asItem());
-				tabData.accept(ExtraIndustrialStuffModItems.STEEL_INGOT.get());
-				tabData.accept(ExtraIndustrialStuffModBlocks.STEEL_CASING.get().asItem());
+				tabData.accept(ExtraIndustrialStuffModItems.STEEL_ARMOR_HELMET.get());
+				tabData.accept(ExtraIndustrialStuffModItems.STEEL_ARMOR_CHESTPLATE.get());
+				tabData.accept(ExtraIndustrialStuffModItems.STEEL_ARMOR_LEGGINGS.get());
+				tabData.accept(ExtraIndustrialStuffModItems.STEEL_ARMOR_BOOTS.get());
 			}).withSearchBar().build());
-
-	@SubscribeEvent
-	public static void buildTabContentsVanilla(BuildCreativeModeTabContentsEvent tabData) {
-
-		if (tabData.getTabKey() == CreativeModeTabs.COMBAT) {
-			tabData.accept(ExtraIndustrialStuffModItems.STEEL_ARMOR_HELMET.get());
-			tabData.accept(ExtraIndustrialStuffModItems.STEEL_ARMOR_CHESTPLATE.get());
-			tabData.accept(ExtraIndustrialStuffModItems.STEEL_ARMOR_LEGGINGS.get());
-			tabData.accept(ExtraIndustrialStuffModItems.STEEL_ARMOR_BOOTS.get());
-		}
-	}
 }
